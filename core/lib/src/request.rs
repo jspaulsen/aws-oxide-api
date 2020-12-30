2409940214f1303313cd4dda506301c5ef46c685
Jacob Paulsen <fatiguely@gmail.com>
2020-12-30 10:30:23 -0800

    Use existing query functionality (#4)
    Signed-off-by: jspaulsen <jpaulsen@lo3energy.com>

--- core/lib/src/request.rs
+++ core/lib/src/request.rs
@@ -1,23 +1,20 @@
-use std::{
-    collections::HashMap,
-    sync::Arc,
-};
+use std::sync::Arc;
 
 use aws_oxide_api_route::IncomingRoute;
 use state::Container;
 use crate::{
-    http::{
-        HeaderMap,
-        Uri,
-    },
+    http::HeaderMap,
     LambdaRequest,
-    netlify_lambda_http::Body,
+    netlify_lambda_http::{
+        Body,
+        RequestExt,
+        StrMap,
+    },
 };
 
 pub struct InnerRequest {
     request: LambdaRequest,
     incoming: IncomingRoute,
-    parameters: HashMap<String, String>,
 }
 
 #[derive(Clone)]
@@ -29,12 +26,10 @@ pub struct OxideRequest {
 impl InnerRequest {
     fn new(request: LambdaRequest) -> Self {
         let incoming = IncomingRoute::from(&request);
-        let parameters = parse_query(&request.uri());
 
         Self {
             request,
             incoming,
-            parameters,
         }
     }
 }
@@ -51,8 +46,11 @@ impl OxideRequest {
         &self.inner.incoming
     }
 
-    pub fn parameters(&self) -> &HashMap<String, String> {
-        &self.inner.parameters
+    pub fn parameters(&self) -> StrMap {
+        self
+            .inner
+            .request
+            .query_string_parameters()
     }
 
     pub fn body(&self) -> &Body {
@@ -62,64 +60,4 @@ impl OxideRequest {
     pub fn headers(&self) -> &HeaderMap {
         &self.inner.request.headers()
     }
-}
-
-
-pub fn parse_query(uri: &Uri) -> HashMap<String, String> {
-    match uri.query() {
-        Some(params) => {
-            let mut ret: HashMap<String, String> = HashMap::new();
-            let split_params: Vec<&str> = params
-                .split('&')
-                .collect();
-
-            for param_str in &split_params {
-                let parsed: Vec<&str> = param_str
-                    .split('=')
-                    .collect();
-
-                let param = parsed.get(0);
-                let value = parsed
-                    .get(1)
-                    .unwrap_or(&"");
-
-                if let Some(param) = param {
-                    ret.insert(
-                        param.to_string(),
-                        value.to_string(),
-                    );
-                }
-            }
-
-            ret
-        },
-        None => HashMap::new(),
-    }
-}
-
-
-#[cfg(test)]
-mod tests {
-    use crate::{
-        http::Uri,
-        request::parse_query,
-    };
-
-    #[test]
-    fn test_parse_query() {
-        let uri = "/foo/bar?abcd=123&defg=456&key#flip".parse::<Uri>()
-            .unwrap();
-
-        let parameters = parse_query(&uri);
-        let empty = parameters
-            .get("key")
-            .unwrap();
-
-
-        assert_eq!(empty, "");
-        assert!(parameters.get("abcd").is_some());
-        assert_eq!(parameters.get("defg").unwrap(), "456");
-        assert!(parameters.get("flip").is_none());
-    }
-
 }
